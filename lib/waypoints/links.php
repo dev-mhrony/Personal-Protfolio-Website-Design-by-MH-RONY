4f92d8b1de6911a59440c2cd4378e99f5526e0c8
dev-mhrony <mh.rony.dhanvi@gmail.com>
2023-02-09 22:55:49 +0600

    Last update - 09-02-2023

--- lib/waypoints/links.php
+++ lib/waypoints/links.php
@@ -3,3 +3,15 @@
     'js' => 'lib/waypoints/waypoints.min.js'
   );
 ?>
+
+
+
+<!-- /******* 
+
+Design By               : MH RONY 
+Website URL             : https://developerrony.com
+GitHub URL              : https://github.com/dev-mhrony
+Yoututbe Channel Link   : https://m.youtube.com/channel/UChYhUxkwDNialcxj-OFRcDw
+Yoututbe Channel Link   : https://www.youtube.com/@codecampbdofficial
+
+********/ -->
